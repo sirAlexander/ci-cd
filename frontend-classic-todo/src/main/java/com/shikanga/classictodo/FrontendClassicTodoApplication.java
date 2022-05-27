c99184dfed555cc9e63c7f895f44ee5c1a784810
Alexander Shikanga-Tindi <alexander.tindi@yapily.com>
2022-05-27 22:31:54 +0100

    initial creation of controller

--- frontend-classic-todo/src/main/java/com/shikanga/classictodo/FrontendClassicTodoApplication.java
+++ frontend-classic-todo/src/main/java/com/shikanga/classictodo/FrontendClassicTodoApplication.java
@@ -1,10 +1,28 @@
 package com.shikanga.classictodo;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Controller
 public class FrontendClassicTodoApplication {
+
+    @Value("${backend.url}")
+    String endpoint;
+    RestTemplate template = new RestTemplate();
+
+
+    @PostConstruct
+    public void postConstruct(){
+
+        System.out.println(" UI initialized for backend at " + endpoint);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(FrontendClassicTodoApplication.class, args);
