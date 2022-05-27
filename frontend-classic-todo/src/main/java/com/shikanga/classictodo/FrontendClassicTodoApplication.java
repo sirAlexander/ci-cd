f4c4a12743fdf213f3d94dde5b76f01922fa7b34
Alexander Shikanga-Tindi <alexander.tindi@yapily.com>
2022-05-27 22:09:34 +0100

    initial project import

---
+++ frontend-classic-todo/src/main/java/com/shikanga/classictodo/FrontendClassicTodoApplication.java
@@ -0,0 +1,13 @@
+package com.shikanga.classictodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendClassicTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendClassicTodoApplication.class, args);
+    }
+
+}
---
+++ frontend-classic-todo/src/test/java/com/shikanga/classictodo/FrontendClassicTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikanga.classictodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendClassicTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
