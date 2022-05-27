4372d35645c8625e30ae9c5144c1cc1c888c03a9
Alexander Shikanga-Tindi <alexander.tindi@yapily.com>
2022-05-27 22:52:57 +0100

    created getItems endpoint

--- frontend-classic-todo/src/main/java/com/shikanga/classictodo/FrontendClassicTodoApplication.java
+++ frontend-classic-todo/src/main/java/com/shikanga/classictodo/FrontendClassicTodoApplication.java
@@ -5,7 +5,10 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -21,6 +24,16 @@ public class FrontendClassicTodoApplication {
     public void postConstruct(){
 
         System.out.println(" UI initialized for backend at " + endpoint);
+    }
+
+    @GetMapping
+    public String getItems(Model model){
+
+        System.out.println(" Invoking: "+ endpoint + "/todos/");
+        ResponseEntity<String[]> response = template.getForEntity(endpoint + "/todos/", String[].class);
+        if(response != null) model.addAttribute("items", response.getBody());
+        return "items";
+
     }
 
 
