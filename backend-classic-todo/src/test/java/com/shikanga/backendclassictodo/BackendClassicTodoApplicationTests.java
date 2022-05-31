be5abcc1ce8ba49690405b2dd2c00d9747df5bd3
Alexander Shikanga-Tindi <alexander.tindi@yapily.com>
2022-05-31 16:34:23 +0100

    initial creation of backend project

---
+++ backend-classic-todo/src/test/java/com/shikanga/backendclassictodo/BackendClassicTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikanga.backendclassictodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClassicTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
